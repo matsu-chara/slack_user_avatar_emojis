13fbc890b69e437d93c6411572538b5003e3df08
matsu-chara <matsuy00@gmail.com>
2017-10-19 10:55:32 +0900

    refactor and add ignore_user filter

--- run.sh
+++ run.sh
@@ -5,16 +5,42 @@ set -o pipefail
 
 source .env
 
-rm -rf "$EMOJI_DIR"; mkdir "$EMOJI_DIR"
+function fetch_users() {
+  curl --silent --show-error "https://slack.com/api/users.list?token=$SLACK_API_TOKEN" | jq -r '.members | map(select(.is_bot == false and .deleted == false and .is_restricted == '"$INCLUDE_RESTRICTED"')) | map(.name + "\t" + .profile.image_72)[]'
+}
 
-curl --silent --show-error "https://slack.com/api/users.list?token=$SLACK_API_TOKEN" | jq -r '.members | map(select(.is_bot == false and .deleted == false and .is_restricted == '${INCLUDE_RESTRICTED}')) | map(.name + "\t" + .profile.image_72)[]' | while read -r name avatar
-do
-  if [[ "$EMOJI_TARGET" == "*" ]] || [[ "$EMOJI_TARGET" == "$name" ]]; then
+function filter_users() {
+  users="$1"
+
+  # filter not target user
+  if [[ "$EMOJI_TARGET" != "*" ]]; then
+    users="$(echo "$users" | grep "$EMOJI_TARGET")"
+  fi
+
+  # filter ignore users
+  join -v 1 <(echo "$users" | sort -u) <(echo "$IGNORE_USERS" | tr ',' '\n' | sort -u)
+}
+
+function fetch_avatar_image() {
+  users="$1"
+
+  ## reset emoji_dir
+  rm -rf "$EMOJI_DIR"
+  mkdir "$EMOJI_DIR"
+
+  ## fetch avatars
+  echo "$users" | while read -r name avatar
+  do
     first=$(echo "$name" | cut -c1)
     last=$(echo "$name" | cut -c2-)
     file_name="${first}_${last}"
     wget --no-verbose --output-document "$EMOJI_DIR/$file_name.jpg" "$avatar"
-    sleep 1
-  fi
-done
+    sleep 0.5
+  done
+}
+
+users=$(fetch_users)
+filtered_users=$(filter_users "$users")
+fetch_avatar_image "$filtered_users"
 python ./slack-emojinator/upload.py "${EMOJI_DIR}"/*
+
