55639b0ce3b38c47d74dfe17098eeda11459ad02
matsu-chara <matsuy00@gmail.com>
2017-10-19 10:50:09 +0900

    support restricted option

--- run.sh
+++ run.sh
@@ -6,7 +6,8 @@ set -o pipefail
 source .env
 
 rm -rf "$EMOJI_DIR"; mkdir "$EMOJI_DIR"
-curl --silent --show-error "https://slack.com/api/users.list?token=$SLACK_API_TOKEN" | jq -r '.members | map(select(.is_bot == false)) | map(.name + "\t" + .profile.image_72)[]' | while read -r name avatar
+
+curl --silent --show-error "https://slack.com/api/users.list?token=$SLACK_API_TOKEN" | jq -r '.members | map(select(.is_bot == false and .deleted == false and .is_restricted == '${INCLUDE_RESTRICTED}')) | map(.name + "\t" + .profile.image_72)[]' | while read -r name avatar
 do
   if [[ "$EMOJI_TARGET" == "*" ]] || [[ "$EMOJI_TARGET" == "$name" ]]; then
     first=$(echo "$name" | cut -c1)
